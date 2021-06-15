a9600e2f3729e1ed1af1b11330e7494367393b36
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-15 16:38:43 +0200

    Interfaccia verso il DB
    Creazione dell'interfaccia verso il db con relativa esportazione per rendere disponibile la funzione query

---
+++ db.js
@@ -0,0 +1,18 @@
+//import dei componenti utili alla connessione e alla configurazione della connessione verso il db
+
+const mysql = require('mysql2/promise');
+const config = require('config');
+
+//funzione per effettuare le query, prende come parametri sql e params
+//con sql la query sql con i punti interrogativi al posto delle variabili e params come parametri della stessa
+async function query(sql, params) {
+  const connection = await mysql.createConnection(config.db);
+  const [results, ] = await connection.execute(sql, params);
+
+  return results;
+}
+
+//export del modulo
+module.exports = {
+  query
+}
