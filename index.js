274c50213add18892975d2d7cea1f6bd4cba4988
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-15 16:37:05 +0200

    Introduzione delle variabili di ambiente
    Aggiunte le variabili di ambiente e dell'estensione dotenv per usarle

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 var path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
