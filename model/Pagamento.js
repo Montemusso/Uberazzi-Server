77a4980add3b7045a01b90d48fbef84bc3a59b3a
StefoXCaffeine <stefano.corrao@yahoo.it>
2021-06-16 15:20:33 +0200

    Create Pagamento.js

---
+++ model/Pagamento.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+    const Pagamento = sequelize.define("Pagamento",{
+        IDPagamento:{
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        IDPrenotazione:{
+            type: Sequelize.INTEGER
+        },
+        DataOra:{
+            type: Sequelize.DATE
+
+        },
+        Importo:{
+            type: STRING           
+        }
+    });
+
+    return Pagamento;
+}
