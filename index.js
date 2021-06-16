7fdb6d171027c8480ed379c320cd06093e5e9445
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-16 10:52:58 +0200

    Aggiunta di multer
    Aggiunta del modulo multer per gli upload delle foto, con relativa configurazione di base e implementazione minima nel verbo post della route parcheggio

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
-var path = require('path');
+//https://www.npmjs.com/package/dotenv
 const dotenv = require('dotenv');
 dotenv.config();
-const dbquery = require ('./query.js')
+//Configurazione per la porta del server
+const port = process.env.PORT;
+var path = require('path');
+//oggetto per le query al db
+const dbquery = require ('./query.js');
+//oggetto per l'upload dei file e relativa configurazione
+//https://www.npmjs.com/package/multer
+var multer  = require('multer');
+//configurazione per la posizione ed il nome del file
+var storage = multer.diskStorage({
+  destination: "./Public/upload",
+  filename: function (req, file, cb) {
+  cb(null, Date.now() + '-' +file.originalname )
+  }
+  })
+var upload = multer({ storage: storage }).array('file');
 
 
+
+
+//aggiunta delle rotte degli elementi statici all'app
 app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, '/Public/upload')));
+
 
 //Home
 //Non deve essere protetta
@@ -92,7 +111,18 @@ app.route('/posteggi')
         }
       })
     .post(function (req, res, next) {
-        // qui ci vanno le query di invio della gestione veicoli
+      upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+          }
+        else if (err) {
+            return res.status(500).json(err)
+          }
+      return res.status(200).send(req.file)
+      //query per aggiugnere il nome della foto, il path e veicolo nel db
+      //il nome del file si trova in req.file.filename
+      //il path invece in req.file.path
+        })
       });
 
 //Corse divise per città
@@ -152,4 +182,4 @@ app.use(function(req, res, next) {
 
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+  });
