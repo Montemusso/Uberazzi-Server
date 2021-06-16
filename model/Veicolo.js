05784725af9cdefbcf4ba291ff51f8db5fc04721
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-16 15:15:24 +0200

    sequelize veicoli

---
+++ model/Veicolo.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize) => {
+    const Veicolo = sequelize.define("Veicolo", {
+      IDVeicolo: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      TipoVeicolo: {
+        type: Sequelize.STRING
+      },
+      Condizioni: {
+        type: Sequelize.STRING
+      },
+      Prenotabile: {
+        type: Sequelize.BOOLEAN
+      },
+      NumeroPosti: {
+        type: Sequelize.INTEGER
+      },
+      Targa: {
+        type: Sequelize.STRING
+      },
+    });
+  
+    return Veicolo;
+  };
