5206c692774c8a2c7e1efaee4db66b06fa39726e
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-17 09:53:55 +0200

    Cambiato da multer a express-fileupload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //import moduli
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 //https://www.npmjs.com/package/dotenv
 const dotenv = require('dotenv');
@@ -7,13 +8,14 @@ dotenv.config();
 var path = require('path');
 //oggetto per le query al db
 const dbquery = require ('./query.js');
-//oggetto per l'upload dei file e relativa configurazione
-//https://www.npmjs.com/package/multer
-var multer  = require('multer');
 //cors
 const cors = require("cors"); //https://www.npmjs.com/package/cors
 //Utile per creare il body delle req
 const bodyParser = require("body-parser");
+
+const addToDb = require("./controller/upload.photo");
+
+let ts = Date.now();
 
 //import modelli
 const db = require("./model");
@@ -50,14 +52,7 @@ function initial() {
 //Configurazione per la porta del server
 const port = process.env.PORT;
 
-//configurazione per la posizione ed il nome del file
-var storage = multer.diskStorage({
-  destination: "./Public/upload",
-  filename: function (req, file, cb) {
-  cb(null, Date.now() + '-' +file.originalname )
-  }
-  })
-var upload = multer({ storage: storage }).array('file');
+app.use(fileUpload());
 
 var corsOptions = {
   origin: "http://localhost"
@@ -70,151 +65,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-
-
-
-
-
-
-
-
 //aggiunta delle rotte degli elementi statici all'app
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.use(express.static(path.resolve(__dirname, '/Public/upload')));
 
-//Router 
-//Home
-//Non deve essere protetta
-app.route('/home')
-    .get(function (req, res, next) {
-          res.sendFile(path.join(__dirname+'/build/index.html'));
-    });
-
-//Registrazione
-//Non deve essere protetta
-app.route('/registrazione')
-    .get(function (req, res, next) {
-        res.json(await dbquery.getEmail(req.query.email));
-        //query per verificare se l'utente è disponibile
-      })
-    .post(function (req, res, next) {
-        // qui ci vanno le query di invio della registrazione
-      });
-
-//Profilo
-//Da proteggere e da collegare alle info del frontend
-app.route('/profilo')
-    .get(function (req, res, next) {
-        if(req.query.user_id){
-            res.send('user id: '+req.query.user_id);
-            //qui ci vanno le query di get del profilo
-        }
-        else{
-            res.send('Nessun user fornito');
-        }
-      })
-    .post(function (req, res, next) {
-        // qui ci vanno le query di update del profilo
-      });
+//Rotte
+require('./app/routes/auth.route')(app);
+require('./app/routes/user.route')(app);
 
 
-//Prenota e prenotazioni sono da proteggere
-//Prenota permette di fare la prenotazione
-//Prenotazioni di vedere le prenotazioni effettuate e di modificarle
-app.route('/prenota')
-    .get(function (req, res, next) {
-        res.json('Prenota il tuo veicolo');
-        //query per verifica dei mezzi disponibili
-      })
-    .post(function (req, res, next) {
-        // qui ci vanno le query di invio della prenotazione
-      });
-    
-app.route('/prenotazioni')
-    .get(function (req, res, next) {
-        res.json('Lista delle prenotazioni');
-        //query per prendere la lista delle prenotazioni filtrato per idutente
-        //la risposta la mando in json 
-      })
-    .post(function (req, res, next) {
-        // qui ci vanno le query di modifica della prenotazione
-      });     
+app.post('/api/upload', function(req, res) {
+  let sampleFile;
+  let uploadPath;
 
-//Amministrazione
-//Da proteggere assolutamente
-app.route('/amministrazione')
-    .get(function (req, res, next) {
-        res.json('Lista degli utenti');
-        //query per prendere tutti gli utenti registrati
-      })
-    .post(function (req, res, next) {
-        // qui ci vanno le query di invio della modifica dei permessi
-      });
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
 
-//Posteggi
-//Da proteggere
-app.route('/posteggi')
-    .get(function (req, res, next) {
-        if(req.query.idposteggio){
-            res.json('Lista dei veicoli nel posteggio: '+ req.query.idposteggio);
-        //query per prendere i veicoli per posteggio
-    }
-        else{
-            res.json('indicare idposteggio per vedere i veicoli');
-        }
-      })
-    .post(function (req, res, next) {
-      if(req.query.idveicolo){
-        upload(req, res, function (err) {
-          if (err instanceof multer.MulterError) {
-              return res.status(500).json(err)
-            }
-          else if (err) {
-              return res.status(500).json(err)
-            }
-        return res.status(200).send(req.file)
-        //query per aggiugnere il nome della foto, il path e veicolo nel db
-        //il nome del file si trova in req.file.filename
-        //il path invece in req.file.path
-          })
-      }
-      else{
-        res.json('indicare idveicolo per inviare le foto');
-      };
-    });
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + './Public/upload/' + ts +sampleFile.name;
 
-//Corse divise per città
-//Da proteggere
-app.route('/corse') ///corse?citta=palermo
-    .get(function (req, res, next) {
-        if(req.query.citta){
-            res.json('Lista delle corse per città: '+ req.query.citta);
-        //query per prendere le corse per città
-    }
-        else{
-            res.json('indicare citta per vedere le corse');
-        }
-      })
-    .post(function (req, res, next) {
-        // qui ci vanno le query di invio della gestione corse
-      });
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
 
-//Upload di media
-//Da proteggere
-app.route('/media') ///media
-    .get(function (req, res, next) {
-        if(req.query.citta){
-            res.json('inserire idveicolo: '+ req.query.idveicolo);
-        //query per prendere le foro per idveicolo
-    }
-        else{
-            res.json('indicare idveicolo');
-        }
-      })
-    .post(function (req, res, next) {
-        // qui ci vanno le query di upload delle foto nel db e nella cartella public
-      });
+    addToDb(sampleFile.name, uploadPath, req.query.IDVeicolo);
+    res.send('File uploaded!');
+  });
+});
 
 //handle 404
 app.use(function(req, res, next) {
