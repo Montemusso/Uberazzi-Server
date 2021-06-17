13935836b920791558e4985bbae8c25974cc3de8
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-17 10:08:52 +0200

    Aggiunte rotte post e handle 404

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,10 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
+
+
+const addToDb = require("../controller/upload.photo");
+
+let ts = Date.now();
 //In questo modulo vengono definite le rotte verso i controller che dovranno poi servire effettivamente i file
 //qui si verifica se si è un utente autenticato o no ed in caso quale permesso si ha
 module.exports = function(app) {
@@ -10,6 +15,12 @@ module.exports = function(app) {
     );
     next();
   });
+
+  /*                //*
+   *                //*
+   * Richieste get  //*
+   *                //*
+   */               //*
 
   app.get(
     "/api/listaVeicoli",
@@ -43,12 +54,55 @@ module.exports = function(app) {
 
   app.get("/", controller.Homepage);
 
-  app.post(
-    "/api/upload",
-    [authJwt.verifyToken, authJwt.isAddettoParcheggio],
-    
-    );
+  /*                //*
+   *                //*
+   * Richieste post //*
+   *                //*
+   */               //*
 
+  app.post('/api/upload', function(req, res) {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + './Public/upload/' + ts +sampleFile.name;
+  
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+  
+      const dbres = addToDb(sampleFile.name, uploadPath, req.query.IDVeicolo);
+      console.log(dbres);
+      res.send('File uploaded!');
+    });
+  });
+
+  /*                //*
+   *                //*
+   * handle 404     //*
+   *                //*
+   */               //*
+  app.use(function(req, res, next) {
+    res.status(404);
+    // risposta con html
+    if (req.accepts('html')) {
+      res.send('404, pagina non trovata');
+      return;
+    }
+    // risposta con json
+    if (req.accepts('json')) {
+      res.json({ error: '404, pagina non trovata' });
+      return;
+    }
+    // default con txt
+    res.type('txt').send('404, pagina non trovata');
+  });  
 };
 
 
