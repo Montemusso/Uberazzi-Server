6d85ef3436bbc107bc29bd76d042eefcd9f90c39
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-17 08:52:53 +0200

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,15 +1,22 @@
+//Questo file serve solo a verificare che la protezione delle rotte funzioni  
+  
+  
   exports.allAccess = (req, res) => {
-    res.status(200).send("Public Content.");
+    res.status(200).send("Utente non registrato.");
   };
   
   exports.userBoard = (req, res) => {
-    res.status(200).send("User Content.");
+    res.status(200).send("Utente.");
   };
   
   exports.adminBoard = (req, res) => {
-    res.status(200).send("Admin Content.");
+    res.status(200).send("Autista.");
   };
   
   exports.moderatorBoard = (req, res) => {
-    res.status(200).send("Moderator Content.");
+    res.status(200).send("addetto parcheggio.");
+  };
+  
+  exports.moderatorBoard = (req, res) => {
+    res.status(200).send("admin.");
   };
