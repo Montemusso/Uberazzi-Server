0e980c74a613fffbbe61a430fc633b47a9a0afe7
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-25 11:40:37 +0200

    Aggiunte query

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,8 @@
 //query varie
+const { Op } = require("sequelize");
 const path = require('path');
 const db = require("../model");
+
 const Utente = db.Utente;
 const Permesso = db.Permesso;
 const Pagamento = db.Pagamento;
@@ -276,10 +278,48 @@ exports.corse = (req, res) => {
 //inserire id autista in prenotazione con id fornito
 
 //Permessi utenti
-//selezionare tutti gli utenti meno se stessi
+//selezionare tutti gli utenti 
+exports.utenti = (req, res) => {
+  Utenti.findAll()
+    .then(utenti => {
+      if (!utenti) {
+        return res.status(404).send({ message: "nessun utente disponibile." });
+      }
+
+    res.status(200).send({
+      utenti
+    }
+        )})
+    .catch(err => {
+      res.status(500).send({ message: err.message });
+    });
+};
 //aggiornare i permessi dell'utente
 
 //Gestione prenotazioni
-//Selezionare tutte le prenotazioni modificate non complete
-
-  
+//Selezionare tutte le prenotazioni attive non complete
+exports.prenotazioni_attive = (req, res) => {
+  Prenotazione.findAll({
+    where: {
+      IDUtente: req.query.IDUtente,
+      Stato: {[Op.ne]:"completata"}
+    },
+    order:[['DataOra', 'DESC']],
+    include:[{
+      model: Veicolo
+    },{
+      model: TipoVeicolo
+    }],
+    limit:3
+  })
+    .then(prenotazione => {
+      if (!prenotazione) {
+        return res.status(404).send({ message: "nessuna prenotazione effettuata." });
+      }    
+    res.status(200).send(
+      prenotazione
+    )})
+    .catch(err => {
+      res.status(500).send({ message: err.message });
+    });
+};
