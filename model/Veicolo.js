3b174fd8c519741b682cbdd8e0985688d68079af
StefoXCaffeine <stefano.corrao@yahoo.it>
2021-06-25 16:55:43 +0200

    Update Veicolo.js

--- model/Veicolo.js
+++ model/Veicolo.js
@@ -6,9 +6,10 @@ module.exports = (sequelize, Sequelize) => {
         autoIncrement: true,
         primaryKey: true
       },
+      /*
       TipoVeicolo: {
         type: Sequelize.STRING,
-      },
+      },*/
       Condizioni: {
         type: Sequelize.STRING(1000)
       },
