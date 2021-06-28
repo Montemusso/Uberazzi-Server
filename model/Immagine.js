dd031f93d4964bc610b883d2ee1daec5a8f557bf
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-28 10:20:08 +0200

    Update Immagine.js

--- model/Immagine.js
+++ model/Immagine.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, Sequelize) => {
         Path:{
             type: Sequelize.STRING(1000)
         },
+        IDVeicolo:{
+            type: Sequelize.INTEGER
+        }
     });
     return Immagine;
 }
