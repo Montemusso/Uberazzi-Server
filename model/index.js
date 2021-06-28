8c88925dbe26d94a8a0b754c3aac844b7e23e959
Montemusso <58508224+Montemusso@users.noreply.github.com>
2021-06-28 09:18:38 +0200

    Update index.js

--- model/index.js
+++ model/index.js
@@ -38,7 +38,8 @@ db.NotificheRitardo = require("../model/NotificheRitardo.js")(sequelize, Sequeli
 
 /*RELAZIONI*/
 /*db.Permesso.hasMany(db.Utente, {foreignKeyConstraint: true });*/
-db.Permesso.hasMany(db.Utente, {foreignKey: 'IDPermesso',  foreignKeyConstraint: true});  //RELAZIONE OK
+//db.Permesso.hasMany(db.Utente, {foreignKey: 'IDPermesso',  foreignKeyConstraint: true});  //RELAZIONE OK
+db.Permesso.belongsTo(db.Utente, {foreignKey: 'IDPermesso',  foreignKeyConstraint: true});
 
 /*db.Utente.hasMany(db.Prenotazione, {foreignKey: 'IDUtente', foreignKeyConstraint: true});*/
 db.Prenotazione.belongsTo(db.Utente, {foreignKey: 'IDCliente', foreignKeyConstraint: true}); //RELAZIONE OK
