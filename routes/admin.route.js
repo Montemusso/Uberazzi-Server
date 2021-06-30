1449384912bbe386321bf02475e819df7c220209
Giuseppe Ganci <73232718+Gancidev@users.noreply.github.com>
2021-06-30 10:24:11 +0200

    Update admin.route.js
    Cambiati i percorsi per salvare i file e anche per metterli nel DB.

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -66,14 +66,15 @@ module.exports = function(app) {
   
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     sampleFile = req.files.immagine;
-    uploadPath = '/home/uberazzi/codice/server/Public/upload/' + ts +sampleFile.name;
+    uploadPath = '/home/uberazzi/codice/server/build/Public/upload/' + ts +sampleFile.name;
+    dbPath = 'Public/upload/' + ts +sampleFile.name;
   
     // Use the mv() method to place the file somewhere on your server
     sampleFile.mv(uploadPath, function(err) {
       if (err)
         return res.status(500).send(err);
   
-      const dbres = addToDb.uploadPhoto(sampleFile.name, uploadPath, req.query.IDVeicolo);
+      const dbres = addToDb.uploadPhoto(sampleFile.name, dbPath, req.query.IDVeicolo);
       //console.log(dbres);
       res.send({message:'File caricato!'});
     });
